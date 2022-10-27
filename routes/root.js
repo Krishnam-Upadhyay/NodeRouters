79c98a03c35f1717a4bf8c1508376b740acf66c3
Krishnam Upadhyay <upadhyaykrishnam21@gmail.com>
2022-10-27 15:35:16 +0530

    Add files via upload

---
+++ routes/root.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+router.get('^/$|/index(.html)?',(req,res)=>{
+    res.sendFile(path.join(__dirname,"..",'views','index.html'));
+
+});
+router.get('/newPage(.html)?',(req,res)=>{
+    res.sendFile(path.join(__dirname,"..",'views','newPage.html'));
+
+});
+router.get('/oldPage(.html)?',(req,res)=>{
+    res.redirect(301,'/newPage.html');
+
+});
+module.exports = router;
